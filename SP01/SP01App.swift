4b1f183d19472cc02ed29c16b645a57f32c6e260
Sanaj Pokharel <sanazpokharel@gmail.com>
2026-01-31 14:05:33 +1100

    Initial Commit

---
+++ SP01/SP01App.swift
@@ -0,0 +1,17 @@
+//
+//  SP01App.swift
+//  SP01
+//
+//  Created by Sanaj Pokharel on 31/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct SP01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
